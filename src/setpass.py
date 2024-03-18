dc25d5d2e9553f58d4c0323691441cced762f8c9
Marko Bago <mbago@cantabpi.com>
2024-03-18 08:21:20 +0100

    Init repo

--- src/setpass.py
+++ src/setpass.py
@@ -1,9 +1,6 @@
 import getpass
 import os
 
-openai = getpass.getpass("openai api pass:")
-pc = getpass.getpass("pinecone api pass:")
+openai_pass = getpass.getpass("openai api pass:")
+os.environ["OPENAI_API_KEY"] = openai_pass
 
-os.environ["OPENAI_API_KEY"] = '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'
-os.environ['PINECONE_API_KEY'] = '582a0d83-c1c8-4b92-94b9-675284ad59c8'
-os.environ['PINECONE_INDEX_NAME'] = 'starter-index'
