e7f0154bc15b6ec884ad665cf1009a07fc846c3e
Marko Bago <mbago@cantabpi.com>
2024-03-18 08:22:57 +0100

    Init repo

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings
 
 # Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
-llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
